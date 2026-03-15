2e3d3be1071543cd3a6a7b4bd3e25fadee04625f
Manueljose22 <manuelleonel73@gmail.com>
2026-03-15 16:06:23 +0100

    feat: configurações inicias

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import Index from './routes/Index'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Index />
+    </>
+  )
+}
+
+export default App
